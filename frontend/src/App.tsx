077475b73c186982e402b9ccc5c185096c159728
Zen-333 <zenhamam56@gmail.com>
2026-04-06 17:43:48 +0100

    hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/App.css'
 import Header from "./component/header"
+import Hero from "./component/hero"
 
 function App() {
 
   return (
     <> 
     <Header/>
+    <Hero/>
     </>
   )
 }
---
+++ frontend/src/component/hero.tsx
@@ -0,0 +1,17 @@
+import "../styles/hero.css";
+
+function Hero() {
+
+  return (
+    <> 
+        <div className="hero">
+            <div className="container-column">
+                <button className="addRecipe-btn-container">+ Add Recipe</button>
+                <p>Start by adding your first recipe</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Hero
