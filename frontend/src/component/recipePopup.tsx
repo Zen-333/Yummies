83b3f83f86544e6cb1fc997e4a1001f4c66db3de
Zen-333 <zenhamam56@gmail.com>
2026-04-09 16:27:54 +0100

    recipe popup

--- frontend/src/component/recipePopup.tsx
+++ frontend/src/component/recipePopup.tsx
@@ -11,9 +11,52 @@ function RecipePopup({onClose}: PopupProps) {
     <div className="popup-overlay" onClick={onClose}>
         <div className="popup" onClick={(e) => e.stopPropagation()}>
             <div className="popup__title">
-                Edit Recipe
+                <h1>Edit Recipe</h1>
             </div>
-            <button onClick={onClose} className="btn btn--secondary">Close</button>
+            <div className="popup__input__block">
+              <div className="popup__input__title">
+                <h2>Recipe Name</h2>
+              </div>
+              <div className="popup__input__field">
+                <input type="text"/>
+              </div>
+            </div>
+            <div className="popup__input__block">
+              <div className="popup__input__title">
+                <h2>Ingredients</h2>
+              </div>
+              <div className="popup__input__field">
+                <input type="text"/>
+              </div>
+            </div>
+             <div className="popup__input__block">
+              <div className="popup__input__title">
+                <h2>Steps</h2>
+              </div>
+              <div className="popup__input__field">
+                <input type="text"/>
+              </div>
+            </div>
+             <div className="popup__input__block">
+              <div className="popup__input__title">
+                <h2>Images / Videos</h2>
+              </div>
+              <div className="popup__input__field">
+                <input type="text"/>
+              </div>
+            </div>
+             <div className="popup__input__block">
+              <div className="popup__input__title">
+                <h2>Notes</h2>
+              </div>
+              <div className="popup__input__field">
+                <input type="text" className="popup__notes__input"/>
+              </div>
+            </div>
+            <div className="popup__action__buttons">
+              <button onClick={onClose} className="btn btn--primary">Save Changes</button>
+              <button onClick={onClose} className="btn btn--secondary">Cancel</button>
+            </div>
         </div>
     </div>
 
