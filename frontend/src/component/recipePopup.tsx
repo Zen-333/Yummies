ffe7a5ea09c79cc32a6ee04a7a0f6bdfb5166b5d
Zen-333 <zenhamam56@gmail.com>
2026-04-14 11:45:04 +0100

    helper array functions tsx

--- frontend/src/component/recipePopup.tsx
+++ frontend/src/component/recipePopup.tsx
@@ -10,17 +10,23 @@ interface PopupProps {
 function RecipePopup({onClose}: PopupProps) {
 
   const [steps, setSteps] = useState<string[]>([""]);
-  
-  const addStep = () => setSteps([...steps, ""]);
+  const [recipeName, setRecipeName] = useState<string>("");
+  const [recipeMedia, setRecipeMedia] = useState<string[]>([""]);
+  const [recipeIngredients, setRecipeIngredients] = useState<string[]>([""]);
+  const [recipeNotes, setRecipeNotes] = useState<string>("");
 
-  const removeStep = (index: number) => {
-    setSteps(steps.filter((_, i) => i !== index));
+  const addToArray = <T,>(setter: React.Dispatch<React.SetStateAction<T[]>>, defaultValue: T) => {setter((prev) => [...prev, defaultValue])}; 
+
+  const removeFromArray = <T,>(setter: React.Dispatch<React.SetStateAction<T[]>>, index: number) => {
+      setter((prev) => prev.filter((_, i) => i !== index));
   };
 
-  const updateStep = (index: number, value: string) => {
-    const updated = [...steps];
-    updated[index] = value;
-    setSteps(updated);
+  const updateArray = <T,>(setter: React.Dispatch<React.SetStateAction<T[]>>, index: number, value: T) => {
+    setter((prev) => {
+      const updated = [...prev];
+      updated[index] = value;
+      return updated;
+    });
   };
 
 
@@ -38,7 +44,7 @@ function RecipePopup({onClose}: PopupProps) {
 
               <div className="popup__input__block">
                 <p className="popup__input__lable">Recipe Name</p>
-                <input type="text" placeholder="e.g. Classic Pancakes"/>
+                <input type="text" placeholder="e.g. Classic Pancakes" onChange={(e) => setRecipeName(e.target.value)}/>
               </div>
 
               <div className="popup__input__block">
@@ -49,7 +55,7 @@ function RecipePopup({onClose}: PopupProps) {
               <div className="popup__input__block">
                 <div className="popup__input__lable popup__input__lable--row">
                   <p>Steps</p>
-                  <button className="btn btn--secondary" onClick={addStep}>
+                  <button className="btn btn--secondary" onClick={() => addToArray<string>(setSteps, "")}>
                     <FontAwesomeIcon icon={faPlus}/> Add Step
                   </button>
                 </div>
@@ -60,9 +66,9 @@ function RecipePopup({onClose}: PopupProps) {
                       <input type="text" 
                       value={step} 
                       placeholder={`Step ${index + 1}`}
-                      onChange={(e) => updateStep(index, e.target.value)} />
+                      onChange={(e) => updateArray(setSteps, index,e.target.value)} />
 
-                      <button className="btn btn--icon" onClick={() => removeStep(index)}>
+                      <button className="btn btn--icon" onClick={() => removeFromArray(setSteps, index)}>
                         <FontAwesomeIcon icon={faX} />
                       </button>
                     </div>
@@ -83,7 +89,7 @@ function RecipePopup({onClose}: PopupProps) {
 
               <div className="popup__input__block">
                 <p className="popup__input__lable">Notes</p>
-                <textarea rows={3} placeholder="Any extra tips or serving suggestions..."></textarea>
+                <textarea rows={3} placeholder="Any extra tips or serving suggestions..." onChange={(e) => setRecipeNotes(e.target.value)}></textarea>
               </div>
 
             </div>
