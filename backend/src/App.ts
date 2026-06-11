2235bb770b28c5db1871735968876cbdfde79453
Zen-333 <zenhamam56@gmail.com>
2026-06-11 11:21:30 +0100

    Vercel json

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,9 +1,15 @@
 import express, { Application } from 'express';
-
+import cors from 'cors'
 import recipeRoutes from './routes/recipeRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
 const app: Application = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}));
+
 
 app.use(express.json());
 
